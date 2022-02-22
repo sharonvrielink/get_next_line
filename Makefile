42be79531586bd3dd999c44919a9b77b7c73b30c
Sharon@Codam <sharonvrielink@gmail.com>
2022-02-22 09:43:01 +0100

    Makefile + dir src

---
+++ Makefile
@@ -0,0 +1,60 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    Makefile                                           :+:    :+:             #
+#                                                      +:+                     #
+#    By: svrielin <svrielin@student.codam.nl>         +#+                      #
+#                                                    +#+                       #
+#    Created: 2022/02/17 11:55:20 by svrielin      #+#    #+#                  #
+#    Updated: 2022/02/22 09:42:20 by svrielin      ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
+NAME			:=	get_next_line
+CC				:=	gcc
+CFLAGS			?=	-Wall -Wextra -Werror
+
+#################################Project_files##################################
+SRC_DIR			:=	./src
+SRC_FILES		:=	get_next-line.c get_next_line_utils.c
+OBJ_DIR			:=	./obj
+OBJ_FILES		:=	$(addprefix $(OBJ_DIR)/, $(SRC_FILES:.c=.o))
+
+ifdef DEBUG
+CFLAGS	+=	-g
+NAME = get_next_line_debug
+endif
+
+all: $(NAME)
+
+# $@ filename of the target $^ all prerequisites
+# r: uses replacement for the objects files while inserting the files member into archive
+# c: create the library if it does not exist
+# ar -q: quickly append the .o files of ft_printf to the archive libft.a
+# cp: copies libft.a to libftprintf.a
+$(NAME): $(OBJ_FILES)
+	ar rc $@ $^
+	ar -q $(OBJ_FILES)
+	cp $(NAME)
+
+# -p: if parent dirs do not exist, generate them to accommodate 
+# gcc -c: compile but not link the file, makes the result an object file
+# gcc -o: name of the output file
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# rm -df instead of rmdir, because this doesn't give an error when dir doesn't exist
+# -d: dir, removes empty directories
+# -f: force, ignore nonexistant files
+clean:
+	rm -f $(OBJ_FILES) 
+	rm -df $(OBJ_DIR)
+	@echo "Object files and directory removed"
+
+fclean: clean
+	@rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: clean fclean main printf_tester
---
+++ src/get_next_line.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: svrielin <svrielin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/09/22 18:26:17 by svrielin      #+#    #+#                 */
+/*   Updated: 2021/10/06 17:33:38 by svrielin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*create_empty_string(void)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char));
+	if (!str)
+		return (NULL);
+	str[0] = '\0';
+	return (str);
+}
+
+void	strdelete(char **str, int fd)
+{
+	free(str[fd]);
+	str[fd] = create_empty_string();
+}
+
+char	*newline(int fd, char **saved, int bytes_read)
+{
+	char	*line;
+	char	*tmp;
+	int		len;
+
+	if ((ft_strchr(saved[fd], '\n')))
+	{
+		tmp = ft_strdup((ft_strchr(saved[fd], '\n') + 1));
+		len = ft_strlen(saved[fd]) - ft_strlen(tmp);
+		line = ft_strndup(saved[fd], len);
+		free (saved[fd]);
+		saved[fd] = ft_strdup(tmp);
+		free (tmp);
+		return (line);
+	}
+	if (bytes_read < BUFFER_SIZE)
+	{
+		if (bytes_read == 0 && saved[fd][0] == '\0')
+			return (NULL);
+		line = ft_strdup(saved[fd]);
+		strdelete(saved, fd);
+		return (line);
+	}
+	return (NULL);
+}
+
+int	readtillnewline(int fd, char **saved)
+{
+	char	*tmp;
+	int		bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+
+	bytes_read = BUFFER_SIZE;
+	while (!ft_strchr(saved[fd], '\n') && bytes_read == BUFFER_SIZE)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free (saved[fd]);
+			return (0);
+		}
+		buffer[bytes_read] = '\0';
+		tmp = ft_gnlstrjoin(saved[fd], buffer);
+		if (tmp == NULL)
+			return (0);
+		saved[fd] = ft_strdup(tmp);
+		free(tmp);
+	}
+	return (bytes_read);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*saved[OPEN_MAX];
+	char		*line;
+	int			bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!saved[fd])
+	{
+		saved[fd] = create_empty_string();
+		if (saved[fd] == NULL)
+			return (NULL);
+	}
+	bytes_read = readtillnewline(fd, saved);
+	if (bytes_read == 0)
+		return (NULL);
+	line = newline(fd, saved, bytes_read);
+	return (line);
+}
